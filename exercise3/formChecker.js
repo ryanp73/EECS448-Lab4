e4de6b46d117f0ac8dcddc20a83f0cae44602031
Ryan Pope <poperyan73@gmail.com>
2019-03-07 10:00:21 -0600

    Added frontend functionality

---
+++ exercise3/formChecker.js
@@ -0,0 +1,58 @@
+function validate(event) {
+    event.preventDefault();
+    
+    let message = document.getElementById("message");
+    let username = document.getElementById("username").value;
+    let password = document.getElementById("password").value;
+
+    if (username.length == 0) {
+        message.innerText = "Username is empty.";
+        return;
+    }
+    if (password.length == 0) {
+        message.innerText = "Password is empty.";
+        return;
+    }
+
+    // It's not actually possible to catch all email's with regex, but this will do for now.
+    let re = /[a-zA-Z0-9\.]+@[a-zA-Z0-9]+\.[a-zA-Z0-9]+\./;
+
+    if (!re.test(username)) {
+        message.innerText = "Username is empty.";
+        return;
+    }
+    
+    let shipping = null;
+    let shippingOptions = document.getElementsByName("shipping");
+    
+    for (let i = 0; i < 3; i++) {
+        if (shippingOptions[i].checked) {
+            shipping = shippingOptions;
+        }
+    }
+    if (shipping == null) {
+        message.innerText = "Must pick a shipping option.";
+        return;
+    }
+
+    let cube1 = document.getElementsByName("cube1")[0].value;
+    let cube2 = document.getElementsByName("cube2")[0].value;
+    let cube3 = document.getElementsByName("cube3")[0].value;
+
+    if (cube1.length == "" || cube1 < 0) {
+        message.innerText = "Enter a quantity for the White Rubik's Cube";
+        return;
+    }
+
+    if (cube2.length == "" || cube2 < 0) {
+        message.innerText = "Enter a quantity for the American Rubik's Cube";
+        return;
+    }
+
+    if (cube3.length == "" || cube3 < 0) {
+        message.innerText = "Enter a quantity for the 8x8 Rubik's Cube";
+        return;
+    }
+
+    document.getElementById("storeForm").submit();
+}
